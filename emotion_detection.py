4bdc12b658312791ef62366dc3d1ccea1d8606b1
derickdenny <derickdenny16@gmail.com>
2026-03-23 02:01:09 -0400

    task 3

--- emotion_detection.py
+++ emotion_detection.py
@@ -16,4 +16,24 @@ def emotion_detector(text_to_analyze):
     
     response = requests.post(url, json=input_json, headers=headers)
     
-    return response.text
+    formatted_response = json.loads(response.text)
+    
+    emotions = formatted_response['emotionPredictions'][0]['emotion']
+    
+    anger = emotions['anger']
+    disgust = emotions['disgust']
+    fear = emotions['fear']
+    joy = emotions['joy']
+    sadness = emotions['sadness']
+    
+    # Find dominant emotion
+    dominant_emotion = max(emotions, key=emotions.get)
+    
+    return {
+        'anger': anger,
+        'disgust': disgust,
+        'fear': fear,
+        'joy': joy,
+        'sadness': sadness,
+        'dominant_emotion': dominant_emotion
+    }
